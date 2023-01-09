96e5231cb6492c3953657972406ff7945e47eef1
Guerroui Mohammed Naim <mohamednaim.guerroui>
2023-01-10 00:17:05 +0100

    feat: 🎸 (game-play): add fight and arena logic

---
+++ src/game-play/Arena.ts
@@ -0,0 +1,46 @@
+import { Fighter } from "../characters/Fighter";
+import { Zombie } from "../characters/Zombie";
+import { Ninja } from "../characters/Ninja";
+import { Capitaine } from "../characters/Capitaine";
+import { shuffleArray } from "../helper/arrayFunctions";
+import { Fight } from "./Fight";
+
+export class Arena {
+  fightersLife = 10;
+  fighters: Fighter[] = [];
+  fightPairs: fightPair[] = [];
+  initFighters() {
+    this.fighters = [
+      new Zombie(this.fightersLife),
+      new Fighter(this.fightersLife),
+      new Ninja(this.fightersLife),
+      new Capitaine(this.fightersLife),
+    ];
+  }
+
+  getRandomFightsPair() {
+    const fightersShuffled = shuffleArray(this.fighters);
+    let pairs: fightPair[] = [];
+
+    for (var i = 0; i < fightersShuffled.length - 1; i++) {
+      for (var j = i; j < fightersShuffled.length - 1; j++) {
+        pairs.push({
+          fighter1: fightersShuffled[i],
+          fighter2: fightersShuffled[j + 1],
+        });
+      }
+    }
+    this.fightPairs = pairs;
+  }
+
+  startCompetition() {
+    this.fightPairs.forEach((pair) => {
+      new Fight(pair, this.fightersLife).startFight();
+    });
+  }
+}
+
+export type fightPair = {
+  fighter1: Fighter;
+  fighter2: Fighter;
+};
---
+++ src/game-play/Fight.ts
@@ -0,0 +1,50 @@
+import { fightPair } from "./Arena";
+import { Fighter } from "../characters/Fighter";
+export class Fight {
+  fighter1: Fighter;
+  fighter2: Fighter;
+
+  constructor(fighters: fightPair, fightersLife: number) {
+    this.fighter1 = fighters.fighter1;
+    this.fighter2 = fighters.fighter2;
+    this.fighter1.setLife(fightersLife);
+    this.fighter2.setLife(fightersLife);
+  }
+
+  startFight() {
+    console.log(
+      "\n \n \n \n \n",
+      "start fight between",
+      this.fighter1,
+      "and ",
+      this.fighter2
+    );
+
+    while (this.fighter1.getLife() > 0 && this.fighter2.getLife() > 0) {
+      this.fighter2.receiveAttaque(this.fighter1.randomAttack());
+
+      // stop fight if fighter2 is dead
+      if (this.fighter2.getLife() < 1) break;
+
+      this.fighter1.receiveAttaque(this.fighter2.randomAttack());
+    }
+
+    console.log("------------------- Fight finished   -------------------");
+
+    console.log(
+      "------------------- Fighter 1 health -------------------",
+      this.fighter1.getLife()
+    );
+
+    console.log(
+      "------------------- Fighter 2 health -------------------",
+      this.fighter2.getLife()
+    );
+
+    const winner = this.fighter1.getLife() > 0 ? "fighter 1" : "fighter 2";
+    console.log(
+      "------------------- The winner is  -------------------",
+      winner
+    );
+  }
+}
