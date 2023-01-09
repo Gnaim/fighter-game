2e86f0eba77e892423acb6c98c69fde10e4b8611
Guerroui Mohammed Naim <mohamednaim.guerroui>
2023-01-10 00:04:27 +0100

    feat: 🎸 (helper): add typescript functions

---
+++ src/helper/arrayFunctions.ts
@@ -0,0 +1,19 @@
+export function getRandomElement<T>(array: Array<T>): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export function shuffleArray<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
